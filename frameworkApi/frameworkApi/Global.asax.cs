65ecc2f6b3f157301a4b5dd3fdb2bccc3107d07e
N2N <320369089@qq.com>
2023-09-06 21:43:12 +0800

    提交

---
+++ frameworkApi/frameworkApi/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using frameworkApi.Service.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace frameworkApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // 初始化websocket
+            var ws = WebSocketServiceImpl.Instance;
+            ws.Start("0.0.0.0", 18001); 
+        }
+    }
+}
---
+++ frameworkApi/frameworkApi/Service/IWebSocketService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frameworkApi.Service
+{
+
+    public interface IWebSocketService
+    {
+
+        void Start(string ip, int port);
+
+        void PushData(string data);
+    }
+}
