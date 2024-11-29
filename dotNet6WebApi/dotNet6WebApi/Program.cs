27b7789b1096216e528a8e19b0e84e0e25eca6af
lixiang <lix.li@ichia.com>
2024-11-29 09:14:35 +0800

    feat：增加windows service 启动

--- dotNet6WebApi/dotNet6WebApi/Program.cs
+++ dotNet6WebApi/dotNet6WebApi/Program.cs
@@ -1,42 +1,59 @@
 using Microsoft.EntityFrameworkCore;
 using Model.Base;
+using NewLife.Log;
 using Service.impl;
 using Service.interfaces;
 using Service.Token;
+using Service.WinSw;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContextFactory<BaseDbContext>(options =>
+XTrace.UseConsole();
+static WebApplication GetApp()
 {
-    options.UseSqlServer(builder.Configuration["ConnectionString:DbConn"]);
-});
+    //这里存放你原有的逻辑
+    var args = Environment.GetCommandLineArgs();
+    var builder = WebApplication.CreateBuilder(args);
 
-// DI注入
-builder.Services.AddSingleton<IRedis, RedisImpl>();
-builder.Services.AddScoped<ITokenHandler, TokenHandler>();
-builder.Services.AddScoped<IUserManagerService, UserService>();
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddScoped<IUserManagerRepository, UserManagerRepository>();
-builder.Services.AddScoped<ITaskRunService, TaskRunImplService>();
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContextFactory<BaseDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration["ConnectionString:DbConn"]);
+    });
+
+    // DI注入
+    builder.Services.AddSingleton<IRedis, RedisImpl>();
+    builder.Services.AddScoped<ITokenHandler, TokenHandler>();
+    builder.Services.AddScoped<IUserManagerService, UserService>();
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services.AddScoped<IUserManagerRepository, UserManagerRepository>();
+    builder.Services.AddScoped<ITaskRunService, TaskRunImplService>();
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+   return app;
+
 }
 
-app.UseAuthorization();
 
-app.MapControllers();
+// 对应着launchSettings.json 的ASPNETCORE_ENVIRONMENT的值
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
 
-app.Run();
+if ("Development".Equals(environment) && args?.Length == 0)
+    args = new string[] { "-run" };
+new WinAgentService { StartAct = GetApp }.Main(args);
